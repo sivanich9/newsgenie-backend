cc7940ab5399b32617891c9ebb70d658656b75a4
prasanna <prasannavenkateshramkumar@gmail.com>
2023-05-09 17:42:52 +0530

    feat: added mongodb atlas and logs

--- database.py
+++ database.py
@@ -1,7 +1,10 @@
 import pymongo
 
+# client = pymongo.MongoClient("mongodb+srv://hanzo:<oassword>@cluster0.13jih.mongodb.net/?retryWrites=true&w=majority")
+
 def get_db(db_name:str):
     client = pymongo.MongoClient("localhost", 27017)
+    # client = pymongo.MongoClient("mongodb+srv://hanzo:<password>@cluster0.13jih.mongodb.net/?retryWrites=true&w=majority")
     db = client.test
     db = client.get_database(db_name)
     # try:

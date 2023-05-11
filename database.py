d3d8be01304592dca75d27b4f6df2ed04d38da5e
Prasanna Venkatesh <prasannavenkatesh.ramkumar@iiitb.org>
2023-05-11 12:05:30 +0530

    fix: merge conflict push

--- database.py
+++ database.py
@@ -4,12 +4,8 @@ import pymongo
 # client = pymongo.MongoClient("mongodb+srv://hanzo:<oassword>@cluster0.13jih.mongodb.net/?retryWrites=true&w=majority")
 
 def get_db(db_name:str):
-<<<<<<< HEAD
     client = pymongo.MongoClient("mongodb:27017")
-=======
-    client = pymongo.MongoClient("localhost", 27017)
     # client = pymongo.MongoClient("mongodb+srv://hanzo:<password>@cluster0.13jih.mongodb.net/?retryWrites=true&w=majority")
->>>>>>> a5342c98ed6562cf5b0fec9ed88f14b85093355d
     db = client.test
     db = client.get_database(db_name)
     # try:
